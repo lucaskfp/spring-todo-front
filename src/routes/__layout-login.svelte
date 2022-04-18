ebd58cee538f3b062c16c23c90ba9748bdf86ff8
Lucas Kennedy <lukakfp@gmail.com>
2022-04-18 11:57:03 -0300

    temp

---
+++ src/routes/__layout-login.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../tailwind.css"
+</script>
+
+
+<h1>layout login</h1>
+
+<slot><!-- optional fallback --></slot>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../tailwind.css"
+</script>
+
+<header>header</header>
+
+<main>
+    <slot />
+</main>
+
+
+<footer>footer</footer>
